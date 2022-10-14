9ad43ee7fd6661056e4b4f56334dc59d4d2d86f3
Bogdan Otava <bogdanotava097@gmail.com>
2022-10-14 15:54:46 +0300

    Arrays, Nested Loops

---
+++ Arrays.java
@@ -0,0 +1,12 @@
+// 'Arrays' are used to store multiple values in a single variable.
+
+public class Arrays {
+    public static void main(String[] args) {
+
+        int[] numbers = { 15, 20, 25, 30, 35 };
+
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.println(numbers[i]);
+        }
+    }
+}
