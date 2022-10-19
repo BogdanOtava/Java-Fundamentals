3a0ac733b660d5ff6e418be1124ace57aa81e489
Bogdan Otava <bogdanotava097@gmail.com>
2022-10-19 14:59:32 +0300

    Added - Everything

---
+++ CodingChallenges/CountWordsInString.java
@@ -0,0 +1,13 @@
+/*
+Question: Count the number of words in a random string.
+*/
+
+public class CountWordsInString {
+    public static void main(String[] args) {
+
+        String aString = "Java is a high-level, class-based, object-oriented programming language that is designed to have as few implementation dependencies as possible.";
+        int countWords = aString.split(" ").length;
+
+        System.out.println(countWords);
+    }
+}
